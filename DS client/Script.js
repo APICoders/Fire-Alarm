9fed38f5ec763e182754f1dbeb66c884481b3a6a
KusalR <KusalRathnakumara@gmail.com>
2020-04-30 13:47:17 +0530

    Status Fix

--- DS client/Script.js
+++ DS client/Script.js
@@ -8,16 +8,16 @@ $(document).ready(function () {
 function getSensorDetails() {
     
     var colorMap = {
-        1 : '#00ff00',
-        2 : '#33cc33',
-        3 : '#ccff33',
-        4 : '#ffcc00',
-        5 : '#ff6600',
-        6 : '#e62e00',
-        7 : '#ff0000',
-        8 : '#cc0000',
-        9 : '#990000',
-        10: '#800000'
+        1 : '#00ff009a',
+        2 : '#33cc33b6',
+        3 : '#ccff33b9',
+        4 : '#ffcc00b6',
+        5 : '#ff6600c7',
+        6 : '#e62e00be',
+        7 : '#ff0000b0',
+        8 : '#cc0000b9',
+        9 : '#990000ad',
+        10: '#800000c2'
     };
     
 
@@ -35,7 +35,7 @@ function getSensorDetails() {
             
                 
 
-                tableBody.append('<tr><td>'+element.id+'</td><td>'+element.roomNo+'</td><td>'+element.floorNo+'</td><td>'+element.status+'</td><td id = "smoke" >'+element.smokeLevel+'</td><td id="co2">'+element.co2Level+'</td></tr>');
+                tableBody.append('<tr><td>'+element.id+'</td><td>'+element.status+'</td><td>'+element.roomNo+'</td><td>'+element.floorNo+'</td><td id = "smoke" >'+element.smokeLevel+'</td><td id="co2">'+element.co2Level+'</td></tr>');
                 
 
                 $('#FireAlarmTable td:nth-child(5)').css('background-color', function(){
@@ -46,49 +46,18 @@ function getSensorDetails() {
                     return colorMap[$(this).text()] || '';
                 });
 
+                var tds = document.querySelectorAll("td:nth-child(2)");
+                            for (var i = 0; i < tds.length; i++){
+                            if (tds[i].firstChild.nodeValue == 0){
+                             tds[i].firstChild.nodeValue = "Inactive";
+                            }else if (tds[i].firstChild.nodeValue == 1) {
+                                tds[i].firstChild.nodeValue = "Active";
+                            }
+                            }
+
          } ) },
         error: function (jqXHR,error,errorThrown) {
             alert(error);
         }
     });
 }
-
-
-// function getSensorDetails(){
-
-//     fetch('http://localhost:8081/FireAlarmMonitor/rest/fireAlarms')
-//     .then((res) => res.json())
-//     .then((data) => {
-//       data.forEach(function(element){
-//         output += '<tr><td>'+element.id+'</td><td>'+element.roomNo+'</td><td>'+element.floorNo+'</td><td>'+element.status+'</td><td>'+element.smokeLevel+'</td><td>'+element.co2Level+'</td></tr>';
-//       });
-//       document.getElementById('output').innerHTML = output;
-//     })
-//   }
-
-$(document).ready(function(){
-    var fireAlarm = {};
-    $('#add').click(function(){
-        fireAlarm.id = 1;
-        fireAlarm.roomNo = 1;
-        fireAlarm.floorNo = 20;
-        fireAlarm.status = 1;
-        fireAlarm.smokeLevel = 2;
-        fireAlarm.co2Level = 1;
-        var fireSMS = JSON.stringify(fireAlarm);
-        $.ajax({
-            url: "http://localhost:8081/FireAlarmMonitor/rest/fireAlarms/sendSMS",
-            type: "POST",
-            data:fireSMS,
-            contentType: "application/json",
-
-            success:function(){
-                alert('Saved Successfully')
-            },
-            error:function(error){
-                alert(error);
-            }
-
-        })
-    })
-})
