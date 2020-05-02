ce199c0f3520991093dd9caa7b869352abdc3f35
pasan pramuditha <pasanpramuditha97@gmail.com>
2020-05-02 21:37:57 +0530

    sms correct

--- FireAlarmMonitor/src/com/ds/FireAlarmMonitor/controller/SmsSender.java
+++ FireAlarmMonitor/src/com/ds/FireAlarmMonitor/controller/SmsSender.java
@@ -10,16 +10,16 @@ import com.twilio.type.PhoneNumber;
 public class SmsSender {
  // Find your Account Sid and Auth Token at twilio.com/console
  public static final String ACCOUNT_SID =
-         "ACae7f0f69b34d4c8724d932ff15b746d7";
+         "AC736d168b60775baa4fe6173695f67dec";
  public static final String AUTH_TOKEN =
-         "3fc0afae787af199383976d18075044f";
+         "557ebf6e19808569e5f18fc4b3dbac9b";
 
  public void sendSMS(FireAlarm sms) {
      Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
      Message message = Message
-             .creator(new PhoneNumber("+94710729569"), // to
-                     new PhoneNumber("+18634171972"), // from
+             .creator(new PhoneNumber("+94766197430"), // to
+                     new PhoneNumber("+12025177685"), // from
                      "Fire Alarm Monitor  Floor NO: " +sms.getFloorNo()+
                       "  Room No: "+sms.getRoomNo()+"  CO2 Level : "+sms.getCo2Level()+"  Smoke Level :"+sms.getSmokeLevel()+"  Fire Alarm Alert...! ")
              .create();
